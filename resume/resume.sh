8f8fa1e33f7fe2c833ace0bec958d4b4d5db4d46
Jay Hannah <jay@jays.net>
2025-05-18 11:07:29 -0500

    Also DOCX

--- resume/resume.sh
+++ resume/resume.sh
@@ -3,7 +3,7 @@
 #   npm install resumed jsonresume-theme-kendall
 npx resumed render resume.json --theme=jsonresume-theme-kendall
 
-# MARKDOWN -> PDF
+# MARKDOWN -> PDF, DOCX
 #   brew install pandoc basictex
 #   sudo ln -s /Library/TeX/Distributions/.DefaultTeX/Contents/Programs/arm64/pdflatex /usr/local/bin
 pandoc resume.md -o resume.pdf
