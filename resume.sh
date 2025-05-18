6f21aa9a1617eac001d2dac109942c3415f64f4e
Jay Hannah <jay@jays.net>
2025-05-18 10:56:53 -0500

    Let's move everything to a subdir

--- resume.sh
+++ resume.sh
@@ -1,4 +1,11 @@
-# brew install basictex
-# sudo ln -s /Library/TeX/Distributions/.DefaultTeX/Contents/Programs/arm64/pdflatex /usr/local/bin
+# JSON -> HTML
+#   https://github.com/rbardini/resumed
+#   npm install resumed jsonresume-theme-kendall
+#   npx resumed render resume.json --theme=jsonresume-theme-kendall
+
+# MARKDOWN -> PDF
+#   brew install pandoc basictex
+#   sudo ln -s /Library/TeX/Distributions/.DefaultTeX/Contents/Programs/arm64/pdflatex /usr/local/bin
 pandoc resume.md -o resume.pdf
+pandoc resume.md -o resume.docx
 
